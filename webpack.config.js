cdb9cfe04276bdaa55f055a366c6a27a15d96fba
alpenfritz <mymailismissing@gmail.com>
2018-02-12 17:06:29 +0900

    Create Hello World

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-1'],
+        },
+      },
+    ],
+  },
+  watch: true,
+};
