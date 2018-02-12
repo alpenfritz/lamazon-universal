1f9b8dbfe5155941cbc2963fdb8650508b91f539
alpenfritz <mymailismissing@gmail.com>
2018-02-12 23:36:33 +0900

    Change version of packages

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react', 'stage-1'],
+          presets: ['env', 'react', 'stage-1'],
         },
       },
     ],
